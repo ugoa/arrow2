de222f3eacfca350040166fe05f06236c3cb4151
Dawei Hu <hoodavy@gmail.com>
2023-11-21 10:25:00 +0800

    metadata to columnarAnyBuffer

--- src/io/odbc/read/mod.rs
+++ src/io/odbc/read/mod.rs
@@ -5,33 +5,29 @@ mod schema;
 pub use deserialize::deserialize;
 pub use schema::infer_schema;
 
-use super::api;
+pub use super::api::buffers::{BufferDesc, ColumnarAnyBuffer};
+pub use super::api::ColumnDescription;
+pub use super::api::Error;
+pub use super::api::ResultSetMetadata;
 
 /// Creates a [`api::buffers::ColumnarBuffer`] from the metadata.
 /// # Errors
-/// Iff the driver provides an incorrect [`api::ResultSetMetadata`]
+/// If the driver provides an incorrect [`api::ResultSetMetadata`]
 pub fn buffer_from_metadata(
-    resut_set_metadata: &impl api::ResultSetMetadata,
-    max_batch_size: usize,
-) -> std::result::Result<api::buffers::ColumnarBuffer<api::buffers::AnyColumnBuffer>, api::Error> {
+    resut_set_metadata: &impl ResultSetMetadata,
+    capacity: usize,
+) -> std::result::Result<ColumnarAnyBuffer, Error> {
     let num_cols: u16 = resut_set_metadata.num_result_cols()? as u16;
 
-    let descs = (0..num_cols)
-        .map(|index| {
-            let mut column_description = api::ColumnDescription::default();
+    let col_descs = vec![ColumnDescription::default(); num_cols as usize];
 
-            resut_set_metadata.describe_col(index + 1, &mut column_description)?;
+    for (i, mut col_desc) in col_descs.iter().enumerate() {
+        resut_set_metadata.describe_col((i + 1) as u16, &mut col_desc)?;
+    }
 
-            Ok(api::buffers::BufferDescription {
-                nullable: column_description.could_be_nullable(),
-                kind: api::buffers::BufferKind::from_data_type(column_description.data_type)
-                    .unwrap(),
-            })
-        })
-        .collect::<std::result::Result<Vec<_>, api::Error>>()?;
+    let descs = col_descs.into_iter().map(|description| {
+        BufferDesc::from_data_type(description.data_type, description.could_be_nullable()).unwrap()
+    });
 
-    Ok(api::buffers::buffer_from_description(
-        max_batch_size,
-        descs.into_iter(),
-    ))
+    Ok(ColumnarAnyBuffer::from_descs(capacity, descs))
 }
